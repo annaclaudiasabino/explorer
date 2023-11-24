c1dbfe50dc0240bfc2d9997a10880d927ac1ef87
annaclaudiasabino <annassabino@gmail.com>
2023-11-24 19:45:04 -0300

    Exercicios de JS

---
+++ stage04/lessons/ask_name/script.js
@@ -0,0 +1,20 @@
+// Exercicio 01
+// let nome = prompt("Qual é seu nome?");
+// alert(`Seja bem vindo ${nome}`);
+
+// Exercicio 02
+// let alertSoma = alert("Digite números para fazermos a soma");
+// let soma01 = prompt("Digite o primeiro numero da soma");
+// let soma02 = prompt("Digite o segundo número da soma");
+
+
+// let result = alert("O resultado da soma é " + (Number(soma01) + Number(soma02)));
+
+
+// Exercicio 03
+
+// let alertStart = alert("Digite o primeiro número, o fator de cálculo, e o ultimo número");
+
+// // modo 1 de fazer:
+// let calc = prompt("Digite seu calculo");
+// let result = alert(eval(calc));
